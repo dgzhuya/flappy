82f9e5a454a5e8cc9b5496aa2a381d462bb22926
dgzhuya <chenbiuxiu@gmail.com>
2023-03-05 21:59:48 +0800

    🎉 init: 游戏基础框架完成

---
+++ src/main.rs
@@ -0,0 +1,189 @@
+use bracket_lib::prelude::*;
+
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HETGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(10, self.y, YELLOW, BLACK, to_cp437('@'))
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+
+        if self.y < 0 {
+            self.y = 0
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0
+    }
+}
+
+struct State {
+    player: Player,
+    frame_time: f32,
+    mode: GameMode,
+    obstacle: Obstacle,
+    socre: i32,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
+            mode: GameMode::Menu,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
+            socre: 0,
+        }
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.resatrt(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move()
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap()
+        }
+
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press Space to Flap");
+        ctx.print(0, 1, &format!("Score: {}", self.socre));
+
+        self.obstacle.render(ctx, self.player.x);
+        if self.player.x > self.obstacle.x {
+            self.socre += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.socre)
+        }
+
+        if self.player.y > SCREEN_HETGHT || self.obstacle.hit_obstacle(&self.player) {
+            self.mode = GameMode::End
+        }
+    }
+
+    fn resatrt(&mut self) {
+        self.mode = GameMode::Playing;
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
+        self.socre = 0
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You are dead");
+        ctx.print_centered(6, &format!("Your earned {} points", self.socre));
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.resatrt(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::End => self.dead(ctx),
+        }
+    }
+}
+
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score),
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        let screen_x = self.x - player_x;
+        let half_size = self.size / 2;
+
+        for y in 0..self.gap_y - half_size {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'))
+        }
+
+        for y in self.gap_y + half_size..SCREEN_HETGHT {
+            ctx.set(screen_x, y, RED, BLACK, to_cp437('|'))
+        }
+    }
+
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let dose_x_match = player.x == self.x;
+        let play_above_gap = player.y < self.gap_y - half_size;
+        let play_below_gap = player.y > self.gap_y + half_size;
+        dose_x_match && (play_above_gap || play_below_gap)
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+    main_loop(context, State::new())
+}
